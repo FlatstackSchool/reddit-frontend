46e0fd06c471d3a6da5bb3b22b62e82985abbf67
Ainur Akhmetov <akhmetovainur@gmail.com>
2019-01-04 14:12:05 +0300

    add test connection to api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    loading: false,
+    error: false,
+    data: [],
+  };
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  fetch = () => {
+    this.setState({
+      data: [],
+      loading: true,
+      error: false,
+    });
+    axios
+        .get(`https://www.reddit.com/hot.json`, {
+          params: {
+            // apikey: process.env.file_name,
+          },
+        })
+        .then((response) => {
+          this.setState({
+            loading: false,
+            data: [response.data.data.after],
+          });
+        })
+        .catch(() => {
+          this.setState({
+            loading: false,
+            error: true,
+          });
+        });
+  };
   render() {
+    const { data, loading, error } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+            <div>
+              {loading && <p>Loading...</p>}
+              {error && (
+                <div>
+                   <p>Download error</p>
+                   <button onClick={this.fetch}>Try again</button>
+                </div>
+              )}
+              <p>response: {data}</p>
+              {console.log(data)}
+            </div>
+          </header>
       </div>
     );
   }
