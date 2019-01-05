fb169845c586257fa241e317c4c222992d4227d1
Ainur Akhmetov <akhmetovainur@gmail.com>
2019-01-05 19:20:12 +0300

    conflicts resolved

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class HomePage extends Component {
+    state = {
+        loading: false,
+        error: false,
+        data: [],
+    };
+
+    componentDidMount() {
+        this.fetch();
+    }
+
+    fetch = () => {
+        this.setState({
+            data: [],
+            loading: true,
+            error: false,
+        });
+        axios
+            .get(`https://www.reddit.com/hot.json`, {
+                params: {
+                    // apikey: process.env.file_name,
+                },
+            })
+            .then((response) => {
+                this.setState({
+                    loading: false,
+                    data: [response.data.data.children[0].data.author],
+                });
+            })
+            .catch(() => {
+                this.setState({
+                    loading: false,
+                    error: true,
+                });
+            });
+    };
+    render() {
+        const { data, loading, error } = this.state;
+        return (
+            <div>
+                <div>
+                    {loading && <p>Loading...</p>}
+                    {error && (
+                        <div>
+                            <p>Download error</p>
+                            <button onClick={this.fetch}>Try again</button>
+                        </div>
+                    )}
+                    <p>response: {data}</p>
+                    {console.log(data)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
