58f2053c46db3410af78ab528cf7efa1345037c4
Ainur Akhmetov <akhmetovainur@gmail.com>
2019-01-10 20:40:08 +0300

    App.jsx changed to ES6 arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
-class App extends Component {
-  render() {
-    return (
-        <BrowserRouter>
-          <Switch>
+const App = () => (
+    <BrowserRouter>
+        <Switch>
             <Route path="/" component={HomePage} />
-          </Switch>
-        </BrowserRouter>
-    );
-  }
-}
+        </Switch>
+    </BrowserRouter>
+);
 
 export default App;
