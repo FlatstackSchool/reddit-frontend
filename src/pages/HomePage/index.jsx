c9b5b7522eee3c6672e89272c81d564b1a65e5f7
Victor <viktorsorlov@gmail.com>
2019-01-10 22:22:29 +0300

    first test material-ui

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import MainTemplate from '../../templates/MainTemplate';
 
-const HomePage = () => <p>home page</p>;
+const theme = createMuiTheme();
+
+const HomePage = () => (
+  <ThemeProvider theme={theme}>
+    <MainTemplate />
+  </ThemeProvider>
+);
 
 export default HomePage;
