046d418045b26987ff07d24dd97443066faa2fa8
Victor <viktorsorlov@gmail.com>
2019-01-16 18:35:13 +0300

    create card news

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import MainTemplate from '../../templates/MainTemplate';
+import NewsCard from '../../organizm/news-card/NewsCard';
 
 const theme = createMuiTheme();
 class HomePage extends React.Component {
@@ -57,6 +58,13 @@ class HomePage extends React.Component {
             )}
             <p>response: {data}</p>
             {console.log(data)}
+            <NewsCard 
+            avatarImg="https://sun9-29.userapi.com/c845121/v845121770/17f149/6TqH6c5o6nc.jpg?ava=1"
+            userName="BigDaddyLaowai"
+            pubDate="September 14, 2018"
+            title="Путь к техническому лидерству: как разработчику стать техлидом"
+            img="https://tproger.ru/wp-content/uploads/2017/08/coding-mini-js.png"
+            commentsCount="47"/>
           </div>
         </div>
       </MainTemplate>
