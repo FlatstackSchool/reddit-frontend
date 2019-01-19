9e067dd0aa94979b9b077c151cf2ec426ad35961
Regina.Gimazova <Regina.Gimazova@tatar.ru>
2019-01-19 21:08:35 +0300

    oauth window for authentication, not completed

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import React from 'react';
 import axios from 'axios';
 import { createMuiTheme } from '@material-ui/core/styles';
@@ -28,7 +29,7 @@ class HomePage extends React.Component {
           // apikey: process.env.file_name,
         },
       })
-      .then((response) => {
+      .then(response => {
         this.setState({
           loading: false,
           data: [response.data.data.children[0].data.author],
@@ -41,27 +42,73 @@ class HomePage extends React.Component {
         });
       });
   };
+
+  openWindow = () => {
+    const width = 600;
+    const height = 600;
+    const left = window.innerWidth / 2 - width / 2;
+    const top = window.innerHeight / 2 - height / 2;
+    const clientId = process.env.REACT_APP_CLIENT_ID;
+    // генерировать новый state при каждой аутентификации
+    const state = process.env.REACT_APP_STATE;
+    const redirectUri = process.env.REACT_APP_REDIRECT_URI;
+    const baseUrl = process.env.REACT_APP_BASEURL;
+    const url = `${baseUrl}authorize?client_id=${clientId}
+    &response_type=code&state=${state}&redirect_uri=${redirectUri}
+    &duration=temporary&scope=identity`;
+    return window.open(
+      url,
+      'oauth',
+      `width=${width}, height=${height}, left=${left}, top=${top}`,
+    );
+  };
+
+  getRequest = () => {
+    const window = this.openWindow();
+    this.checkAuthWindow(window);
+  };
+
+  checkAuthWindow = window => {
+    const check = setInterval(() => {
+      if (!window || window.closed || window.closed === undefined) {
+        clearInterval(check);
+        console.log('window closed by user');
+      }
+      try {
+        console.log(window.location.hostname);
+        if (window.location.hostname === 'localhost') {
+          window.opener.location.href = window.location.href;
+          window.close();
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }, 500);
+  };
+
   render() {
     const { data, loading, error } = this.state;
     return (
-    <ThemeProvider theme={theme}>
-      <MainTemplate title="Hot">
-        <div>
+      <ThemeProvider theme={theme}>
+        <MainTemplate title="Hot" onclick={this.getRequest}>
           <div>
-            {loading && <p>Loading...</p>}
-            {error && (
-              <div>
-                <p>Download error</p>
-                <button onClick={this.fetch}>Try again</button>
-              </div>
-            )}
-            <p>response: {data}</p>
-            {console.log(data)}
+            <div>
+              {loading && <p>Loading...</p>}
+              {error && (
+                <div>
+                  <p>Download error</p>
+                  <button type="button" onClick={this.fetch}>
+                    Try again
+                  </button>
+                </div>
+              )}
+              <p>response: {data}</p>
+              {console.log(data)}
+            </div>
           </div>
-        </div>
-      </MainTemplate>
-    </ThemeProvider>
-    )
+        </MainTemplate>
+      </ThemeProvider>
+    );
   }
 }
 
