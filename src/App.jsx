53206b65f8c85d41ff62da22ed6889a2c47cb02d
Regina.Gimazova <Regina.Gimazova@tatar.ru>
2019-01-24 19:45:42 +0300

    add redux, react-redux, redux-thunk, getting started with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import HomePage from './pages/HomePage/index';
 import CommentsPage from './pages/CommentsPage/index';
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/comments/:id" component={CommentsPage} exact />
-    </Switch>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/comments/:id" component={CommentsPage} exact />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
