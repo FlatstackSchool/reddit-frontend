efa8bf11e6b85efa158e4ab54a20e2c7618bd2a4
Regina.Gimazova <Regina.Gimazova@tatar.ru>
2019-01-30 12:51:11 +0300

    for redeploy

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -4,7 +4,6 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/index';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk)),
