da6536ab70f6c5c4c5de356cb6a4e7e1df01beb5
Regina.Gimazova <Regina.Gimazova@tatar.ru>
2019-01-30 20:25:07 +0300

    add check condition

--- src/store/actions/checkOauthWindow.jsx
+++ src/store/actions/checkOauthWindow.jsx
@@ -3,13 +3,17 @@ import { CHECK_WINDOW } from './types';
 import { closeOauthWindow } from './closeOauthWindow';
 
 export const checkOauthWindow = window => dispatch => {
-  setInterval(() => {
-    try {
-      if (window.location.pathname.indexOf('callback') >= 0) {
-        dispatch(closeOauthWindow(window));
+  const check = setInterval(() => {
+    if (!window || window.closed || window.closed === undefined) {
+      clearInterval(check);
+    } else {
+      try {
+        if (window.location.pathname.indexOf('callback') >= 0) {
+          dispatch(closeOauthWindow(window));
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
     }
   }, 2000);
   dispatch({
