4b273558d35757b1a08d0d732540626e85c4d6ec
Ainur Akhmetov <akhmetovainur@gmail.com>
2019-02-01 13:02:29 +0300

    added redux to CommentPage

--- src/pages/CommentsPage/index.jsx
+++ src/pages/CommentsPage/index.jsx
@@ -1,59 +1,31 @@
-/* eslint-disable max-len,react/no-unused-state,react/prop-types,consistent-return */
+/* eslint-disable max-len,react/no-unused-state,react/prop-types,consistent-return, react/destructuring-assignment */
 import React from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core';
 import NewsCard from '../../organisms/NewsCard';
 import MainTemplate from '../../templates/MainTemplate';
-import Index from '../../molecules/TimeConverter';
+import TimeConverter from '../../molecules/TimeConverter';
+import receivePosts from '../../store/actions/receivePosts';
 
 const theme = createMuiTheme();
 
 class CommentPage extends React.Component {
-  state = {
-    loading: false,
-    error: false,
-    data: [],
-  };
-
   componentDidMount() {
-    this.fetch();
+    this.props.receivePosts();
   }
 
-  fetch = () => {
-    this.setState({
-      loading: true,
-      error: false,
-    });
-
-    const { match } = this.props;
-
-    axios
-      .get(`https://www.reddit.com/hot.json`)
-      .then(response => {
-        this.setState(() => ({
-          loading: false,
-          data: response.data.data.children.filter(
-            item => item.data.id === String(match.params.id),
-          )[0].data,
-        }));
-      })
-      .catch(() => {
-        this.setState({
-          loading: false,
-          error: true,
-        });
-      });
-  };
-
   render() {
-    const { data, loading, error } = this.state;
-
+    const { data, isLoading, error, match } = this.props;
     if (data) {
+      const post = data.filter(
+        item => item.data.id === String(match.params.id),
+      )[0].data;
       let imgUrl = '';
-      const flag = data.preview;
+      const flag = post.preview;
       if (flag) {
-        imgUrl = data.preview.images[0].source.url.replace(
+        imgUrl = post.preview.images[0].source.url.replace(
           new RegExp('&amp;', 'g'),
           '&',
         );
@@ -64,23 +36,23 @@ class CommentPage extends React.Component {
       return (
         <ThemeProvider theme={theme}>
           <MainTemplate title="">
-            {loading && <p>Loading...</p>}
+            {isLoading && <p>Loading...</p>}
             {error && (
               <div>
                 <p>Download error</p>
-                <button type="button" onClick={this.fetch}>
+                <button type="button" onClick={() => this.props.receivePosts}>
                   Try again
                 </button>
               </div>
             )}
-            {!loading && (
+            {!isLoading && (
               <NewsCard
                 avatarImg="https://sun9-29.userapi.com/c845121/v845121770/17f149/6TqH6c5o6nc.jpg?ava=1"
-                userName={data.author}
-                pubDate={Index(data.created_utc)}
+                userName={post.author}
+                pubDate={TimeConverter(post.created_utc)}
                 img={imgUrl}
-                title={data.title}
-                commentsCount={String(data.num_comments)}
+                title={post.title}
+                commentsCount={String(post.num_comments)}
               />
             )}
           </MainTemplate>
@@ -90,4 +62,31 @@ class CommentPage extends React.Component {
   }
 }
 
-export default CommentPage;
+const mapStateToProps = state => ({
+  data: state.posts.data,
+  isLoading: state.posts.isLoading,
+  error: state.posts.error,
+});
+
+const mapDispatchToProps = dispatch => ({
+  receivePosts: () => {
+    dispatch(receivePosts());
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CommentPage);
+
+CommentPage.propTypes = {
+  receivePosts: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  error: PropTypes.bool,
+};
+
+CommentPage.defaultProps = {
+  error: false,
+};
